52afb510e673e0691e384b36aad410913be07109
Luan Alfredo Freitas Dias <luanfreitas12@poli.ufrj.br>
2022-01-02 12:01:38 -0300

    ECreating the pizza rn project

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+export function Dashboard() {
+
+  
+    return (
+        <View>
+            <Text>Dashboard</Text>
+        </View>
+    )
+}
